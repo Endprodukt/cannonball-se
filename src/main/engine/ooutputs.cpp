8c75279e1e14dc97640fc534b49ff1061595a933
Endprodukt <o.carell@kabelmail.de>
2026-08-18 22:14:36 +0200

    Fix temp branch and keep rumble at 50%

--- src/main/engine/ooutputs.cpp
+++ src/main/engine/ooutputs.cpp
@@ -297,6 +297,7 @@ void OOutputs::diag_centre(int16_t input_motor, uint8_t hw_motor_limit)
         motor_state      = STATE_DONE;
     }
 }
+
 void OOutputs::diag_done()
 {
     if (counter > 0)
@@ -529,7 +530,7 @@ const static uint8_t MOTOR_VALUES_OFFROAD1[] =
 const static uint8_t MOTOR_VALUES_OFFROAD2[] = 
 {
     0x8, 0x5, 0x8, 0xB, 0x8, 0x5, 0x8, 0xB, 0x8, 0x4, 0x8, 0xC, 0x8, 0x4, 0x8, 0xC,
-    0x8, 0x3, 0x8, 0xD, 0x8, 0x3, 0x8, 0xD, 0x8, 0x2, 0x2, 0x8, 0xE, 0x8, 0x2, 0x8, 0xE,
+    0x8, 0x3, 0x8, 0xD, 0x8, 0x3, 0x8, 0xD, 0x8, 0x2, 0x8, 0xE, 0x8, 0x2, 0x8, 0xE,
 };
 
 const static uint8_t MOTOR_VALUES_OFFROAD3[] = 
@@ -555,7 +556,8 @@ void OOutputs::do_motors(int MODE, int16_t input_motor)
     if (!motor_enabled)
     {
         done();
-        return;    }
+        return;
+    }
 
     // In-Game: Test for crash, skidding, whether car is moving
     if (outrun.game_state == GS_INGAME)
@@ -975,7 +977,8 @@ void OOutputs::done()
 // This is the equivalent to writing to register 0x140003
 void OOutputs::motor_output(uint8_t cmd)
 {
-    if (cmd == MOTOR_OFF || cmd == MOTOR_CENTRE)        return;
+    if (cmd == MOTOR_OFF || cmd == MOTOR_CENTRE)
+        return;
 
     int8_t force = 0;
 
@@ -994,7 +997,8 @@ void OOutputs::motor_output(uint8_t cmd)
 // Deluxe Upright: Vibration Enable Table. 4 Groups of vibration values.
 const static uint8_t VIBRATE_LOOKUP[] = 
 {
-    // SLOW SPEED --------   // MEDIUM SPEED ------    1, 0, 0, 0, 1, 0, 0, 0,  1, 1, 0, 0, 1, 1, 0, 0,
+    // SLOW SPEED --------   // MEDIUM SPEED ------
+    1, 0, 0, 0, 1, 0, 0, 0,  1, 1, 0, 0, 1, 1, 0, 0,
     // FAST SPEED --------   // VERY FAST SPEED ---
     1, 1, 1, 0, 1, 1, 1, 0,  1, 1, 1, 1, 1, 1, 1, 1,
 };
